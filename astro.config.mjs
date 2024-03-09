5455aa85b1952c3438d09a0103407d91cb44f42e
Junseong Park <junseongpark.dev@gmail.com>
2024-03-09 09:06:22 +0900

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://docs.jspark.dev',
   integrations: [
     starlight({
-      title: '가이드북',
+      title: 'Docs',
+      description: '나를 위한 문서',
       social: {
         github: 'https://github.com/jsparkdev',
         'x.com': 'https://x.com/jsparkdev',
@@ -17,6 +19,13 @@ export default defineConfig({
           autogenerate: { directory: 'posts' },
         },
       ],
+      locales: {
+        root: {
+          label: '한국어',
+          lang: 'ko-KR',
+        }},
+        lastUpdated: true,
+        pagination: false
     }),
   ],
 });
