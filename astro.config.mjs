21359c5bf6c018ea76812494f8c75145bc29f425
JsPark <junseongpark.dev@gmail.com>
2024-03-16 15:04:41 +0900

    feat(favicon): update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,37 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://docs.jspark.dev',
-  integrations: [
-    starlight({
-      title: 'Docs',
-      description: '나를 위한 문서',
-      social: {
-        github: 'https://github.com/jsparkdev',
-        'x.com': 'https://x.com/jsparkdev',
-        linkedin: 'https://www.linkedin.com/in/jsparkdev',
-      },
-      sidebar: [
-        {
-          label: 'Guides',
-          autogenerate: { directory: 'guides' },
-        },
-        {
-          label: 'References',
-          autogenerate: { directory: 'references' },
-        },
-      ],
-      locales: {
-        root: {
-          label: '한국어',
-          lang: 'ko-KR',
-        }},
-        lastUpdated: true,
-        pagination: false
-    }),
-  ],
+	site: "https://docs.jspark.dev",
+	integrations: [
+		starlight({
+			logo: { dark: "./src/assets/favicon.svg", light: "./src/assets/favicon-black.svg" },
+			title: "Docs",
+			description: "나를 위한 문서",
+			social: {
+				github: "https://github.com/jsparkdev",
+				"x.com": "https://x.com/jsparkdev",
+				linkedin: "https://www.linkedin.com/in/jsparkdev",
+			},
+			sidebar: [
+				{
+					label: "Guides",
+					autogenerate: { directory: "guides" },
+				},
+				{
+					label: "References",
+					autogenerate: { directory: "references" },
+				},
+			],
+			locales: {
+				root: {
+					label: "한국어",
+					lang: "ko-KR",
+				},
+			},
+			lastUpdated: true,
+			pagination: false,
+		}),
+	],
 });
