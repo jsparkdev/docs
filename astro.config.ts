31a45870a1a4c902aa01635cca2819bb149987f0
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2025-04-01 10:50:03 +0900

    update config

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,17 @@ export default defineConfig({
     starlight({
       title: "Docs",
       pagination: false,
+      locales: {
+        root: {
+          label: "한국어",
+          lang: "ko-KR",
+        },
+      },
+      social: {
+        github: "https://github.com/jsparkdev",
+        "x.com": "https://x.com/jsparkdev",
+        blueSky: "https://bsky.app/profile/jspark.dev",
+      },
       sidebar: [
         { label: "팁", link: "/tips" },
         { label: "안드로이드 개발 자료", link: "/android" },
--- src/content/docs/index.md
+++ src/content/docs/index.md
@@ -1,7 +1,10 @@
 ---
-title: Hello
+title: Welcome to
 description: Junseong Park의 문서 모음
 template: splash
+head:
+  - tag: title
+    content: Junseong Park
 hero:
   tagline: Junseong Park의 문서 모음
   image:
