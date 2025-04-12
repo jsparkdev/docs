1c3673024ba4316bd06e8966f6ea745e72e2e149
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2025-04-12 19:40:46 +0900

    initial commit

---
+++ astro.config.ts
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://docs.jspark.dev',
+	integrations: [
+		starlight({
+			title: 'Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/jsparkdev/docs' }],
+			locales: {
+				root: {
+					label: '한국어',
+					lang: 'ko-KR',
+				},
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					autogenerate: { directory: 'guides' },
+				},
+			],
+		}),
+	],
+});
