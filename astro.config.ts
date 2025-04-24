3c5bbb1049b522b6b3e5b5a1c3a9f5e359e7c226
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2025-04-24 20:11:30 +0900

    chore: setup

--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,36 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
+const sidebar: ReturnType<typeof sidebarItem>[] = [
+  sidebarItem('JavaScript', 'javascript'),
+  sidebarItem('TypeScript', 'typescript'),
+  sidebarItem('React', 'react'),
+  sidebarItem('Node.js', 'nodejs'),
+  sidebarItem('pnpm', 'pnpm'),
+  sidebarItem('CI/CD', 'cicd'),
+  sidebarItem('Astro', 'astro'),
+  sidebarItem('Browser', 'browser'),
+  sidebarItem('Cloudflare', 'cloudflare'),
+  sidebarItem('Open Source Tools', 'tools'),
+]
+
 export default defineConfig({
-	site: 'https://docs.jspark.dev',
-	integrations: [
-		starlight({
-			title: 'Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/jsparkdev/docs' }],
-			locales: {
-				root: {
-					label: '한국어',
-					lang: 'ko-KR',
-				},
-			},
-			sidebar: [
-				{
-					label: '가이드',
-					autogenerate: { directory: 'guides' },
-				},
-			],
-		}),
-	],
-});
+  site: 'https://docs.jspark.dev',
+  integrations: [
+    starlight({
+      title: 'Docs',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/jsparkdev/docs' }],
+      locales: {
+        root: {
+          label: '한국어',
+          lang: 'ko-KR',
+        },
+      },
+      sidebar,
+    }),
+  ],
+})
+
+function sidebarItem(label: string, directory: string) {
+  return { label, autogenerate: { directory }, collapsed: true }
+}
